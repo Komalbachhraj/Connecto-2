1f278764bddaf8407f51fcea58edb41be92cafc3
shruti985 <jainshruti753@gmail.com>
2026-03-18 03:36:00 +0530

    fix-socket-io

--- connect-thrive/src/socket.ts
+++ connect-thrive/src/socket.ts
@@ -2,5 +2,6 @@ import { io } from "socket.io-client";
 
 const SOCKET_URL = "https://connecto-2.onrender.com";
 export const socket = io(SOCKET_URL, {
+  transports: ["websocket"],
   autoConnect: true,
 });
